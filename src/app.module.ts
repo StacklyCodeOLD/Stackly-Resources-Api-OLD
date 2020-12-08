9667b0552c68208ca2510028824858390ab61d5e
José Luis Reyes <joseluis.r85@gmail.com>
2020-12-08 01:18:40 -0600

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { Configuration } from './config/config.keys';
 import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
+import { UserController } from './modules/user/user.controller';
+import { RoleController } from './modules/rol/rol.controller';
 
 @Module({
   imports: [ConfigModule],
+  controllers: [UserController, RoleController],
 })
 export class AppModule {
   static port: number | string;
